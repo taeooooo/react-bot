de48cb43da9e8a51fcd756efa5bd89fd57d6c369
taeoh.kim <taeoh.kim@kakao.com>
2022-12-18 15:50:49 +0900

    프로젝트 세팅

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import Layout from '@components/Layout/Layout'
+
+export default function Home() {
+   return (
+      <Layout>
+         <h1>PickMe</h1>
+      </Layout>
+   )
+}

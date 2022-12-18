6ea4507e49abbf92fee288d6254092d91ccca395
taeoh.kim <taeoh.kim@kakao.com>
2022-12-18 15:55:56 +0900

    tailwind 세팅

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from '@components/Layout/Layout'
 export default function Home() {
    return (
       <Layout>
-         <h1>PickMe</h1>
+         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </Layout>
    )
 }
